372326768b7640b72fd8bdc8085d239cc85bad8a
Jouskaio <jouskaio.me@gmail.com>
2022-06-29 22:08:09 +0200

    Correcting the upload media matter in deployement

--- config/env/production/middlewares.js
+++ config/env/production/middlewares.js
@@ -1,20 +1,6 @@
 module.exports = [
   'strapi::errors',
   'strapi::security',
-  {
-    name: 'strapi::security',
-    config: {
-      contentSecurityPolicy: {
-        useDefaults: true,
-        directives: {
-          'connect-src': ["'self'", 'https:'],
-          'img-src': ["'self'", 'data:', 'blob:', 'ec2-52-214-125-106.eu-west-1.compute.amazonaws.com'],
-          'media-src': ["'self'", 'data:', 'blob:', 'ec2-52-214-125-106.eu-west-1.compute.amazonaws.com'],
-          upgradeInsecureRequests: null,
-        },
-      },
-    },
-  },
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::logger',
