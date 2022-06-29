26004fa04619a65892c1ec78db796e58d887f721
Jouskaio <jouskaio.me@gmail.com>
2022-06-29 21:40:51 +0200

    Adding middlewares.js for production

---
+++ config/env/production/middlewares.js
@@ -0,0 +1,26 @@
+module.exports = [
+  'strapi::errors',
+  'strapi::security',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': ["'self'", 'data:', 'blob:', 'ec2-52-214-125-106.eu-west-1.compute.amazonaws.com'],
+          'media-src': ["'self'", 'data:', 'blob:', 'ec2-52-214-125-106.eu-west-1.compute.amazonaws.com'],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+  'strapi::cors',
+  'strapi::poweredBy',
+  'strapi::logger',
+  'strapi::query',
+  'strapi::body',
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
+];
