71f1ad8025e90100c4dfd0653ffbd0ff4ac23ec3
Jouskaio <jouskaio.me@gmail.com>
2023-03-15 10:16:32 +0100

    Adding middlewares modifications

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,5 +1,19 @@
 module.exports = [
   'strapi::errors',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': ["'self'", 'data:', 'blob:', 'embed.api.video', 'cdn.api.video/vod/'],
+          'frame-src': ["'self'", 'data:', 'blob:', 'embed.api.video'],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
   'strapi::security',
   'strapi::cors',
   'strapi::poweredBy',
