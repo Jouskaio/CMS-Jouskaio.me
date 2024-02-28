444dd52117dfa76729822a29025174c3c6048307
Jouskaio <jouskaio.me@gmail.com>
2024-02-28 20:32:44 +0100

    Make pretty updates

--- config/admin.js
+++ config/admin.js
@@ -1,5 +1,15 @@
+const crypto = require('crypto');
+
 module.exports = ({ env }) => ({
   auth: {
     secret: env('ADMIN_JWT_SECRET', '4f789fcf3955611cc13110ad9536159d'),
   },
+  apiToken: {
+    salt: env('API_TOKEN_SALT')
+  },
+  transfer: {
+    token: {
+      salt: env('TRANSFER_TOKEN_SALT', crypto.randomBytes(16).toString('base64'))
+    }
+  }
 });
--- config/plugins.js
+++ config/plugins.js
@@ -3,4 +3,19 @@ module.exports = ({ env }) => ({
   "strapi-chatgpt": {
     enabled: true,
   },
+  email: {
+    config: {
+      provider: 'sendmail',
+      /*providerOptions: {
+        dkim: {
+          privateKey: env('DKIM_PRIVATE_KEY'),
+          keySelector: 'abcd',
+        },
+      },*/
+      settings: {
+        defaultFrom: 'contact@jouskaio.me',
+        defaultReplyTo: 'contact@jouskaio.me',
+      },
+    },
+  },
 });
